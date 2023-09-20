115b9379138ccf4fbfbcd01ee773c7ddb7a898f7
otoycowboy <dennissmart777@gmail.com>
2023-09-20 16:29:45 +0200

    Task completed and tested

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,35 @@
 package com.epam.mjc;
 
 
+import javax.swing.plaf.IconUIResource;
+
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) throws StudentNotFoundException {
+//        Student studentToFind = Student.getValueOf(studentID);
+        if (Student.getValueOf(studentID) != null) {
+            return Student.getValueOf(studentID);
+        } else {
+            throw new StudentNotFoundException("Could not find student with ID " + studentID);
+        }
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = null;
+            try {
+                student = manager.find(IDs[i]);
+            } catch (StudentNotFoundException e) {
+                System.err.print(e);
+            }
+            if (student != null) {
+                System.out.println("Student name " + student.getName());
+            }
+        }
+
+    }
 }
